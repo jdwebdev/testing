063b1a7bcc521e8816a09e45b6a4aa6dc9caedc8
jdwebdev <jdwebdev974@gmail.com>
2022-02-08 22:24:25 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+const App = () => {
+
+  const [msg, setMsg] = useState('')
+
+  const handleClick = async () => {
+    const data = await fetch('/api/youtube')
+    const json = await data.json()
+    const msg = json.msg
+
+    setMsg(msg)
+  }
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={handleClick}>
+          Say Hello
+        </button>
+        <p>{msg}</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require('path')
+require('dotenv').config();
+
+const PORT = process.env.PORT || 7000;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static('client/build'));
+
+app.get('/api/youtube', (_, res) => { // on a transformé le 1er param req en _ pour montrer qu'on ne l'utilise pas et que c'est voulu (pas un oubli)
+    res.send({
+        msg: "Hello coders !"
+    })
+});
+
+app.get('/*', (_, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`Le serveur est lancé sur le port : ${PORT}`)
+});
